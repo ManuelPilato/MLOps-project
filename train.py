a48d4c60d2a71f37a778f7c5e17263d823afc4ed
Manuel Pilato <102042480+ManuelPilato@users.noreply.github.com>
2026-01-01 18:45:59 +0000

    edited comments in train.py

--- train.py
+++ train.py
@@ -5,10 +5,12 @@ from datasets import load_dataset
 from sklearn.metrics import accuracy_score, classification_report
 from transformers import pipeline
 
+#loading the model from huggingn face
 def download():
     print("loading the model from hugging face...\n")
     try:
         #text-classification o sentiment-analysis?
+        #link in the requirements of the project
         model = pipeline("sentiment-analysis", "cardiffnlp/twitter-roberta-base-sentiment-latest")
         return model
     except Exception as e:
@@ -17,6 +19,8 @@ def download():
 
     return model
 
+
+#saving the model locally in the folder sentiment_model
 def save(model):
     output_dir = "./sentiment_model"
     print(f"saving model in {output_dir}")
@@ -25,6 +29,7 @@ def save(model):
     print("Model saved correctly.")
 
 
+#accuracy tests of the model using another dataset
 def test_dataset(model):
 
     #dict
@@ -39,19 +44,23 @@ def test_dataset(model):
 
     subset = ds.shuffle(seed=42).select(range(100))
     
+    #print(type(subset[0]))
     #for i in range(0,10):
     #    print(subset[i])
 
+
+    #splitting 2 columns of the dataset 
     texts = list(subset["text"])         #tweets
     true_labels = list(subset["label"])  #results
     
+    #calculating the predictions with the model only passing the texts 
     predictions = model(texts)
     
     #conversion between words and numbers
     pred_labels = []
     for res in predictions:
         txt_label = res['label'] #'positive'
-        pred_id = LABEL_TO_ID.get(txt_label) #translate
+        pred_id = LABEL_TO_ID.get(txt_label) #translate with dict
         pred_labels.append(pred_id)
 
     # Calculate metrics difference from true ones and predicted
@@ -72,6 +81,7 @@ if __name__ == "__main__":
     accuracy = test_dataset(model)
     ACCURACY_THRESHOLD = 0.6
     print(f"[*] Quality Check: Is Accuracy ({accuracy:.2f}) > Threshold ({ACCURACY_THRESHOLD})?")
+    #if the accuracy is wrose than 60% than it will launch an error in the github unit tests
     assert accuracy >= ACCURACY_THRESHOLD, f"PIPELINE FAILED: Model accuracy {accuracy:.2f} is below threshold {ACCURACY_THRESHOLD}"
     print("[OK] Quality Check Passed.")
 
